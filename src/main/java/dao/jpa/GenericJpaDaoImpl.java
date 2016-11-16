deb6693ddb154e01a0ac5296913b0985b8372629
Artem Lodygin <aldaer@yandex.ru>
2016-11-16 16:02:45 +0300

    further generify Dao implementation

--- src/main/java/dao/jpa/GenericJpaDaoImpl.java
+++ src/main/java/dao/jpa/GenericJpaDaoImpl.java
@@ -3,9 +3,11 @@ package dao.jpa;
 import dao.GenericDao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.Collection;
 
 public abstract class GenericJpaDaoImpl<T> extends AbstractJpaDao implements GenericDao<T> {
-
+    protected abstract Class<T> workingClass();
 
     @Override
     public T save(T record) {
@@ -18,5 +20,28 @@ public abstract class GenericJpaDaoImpl<T> extends AbstractJpaDao implements Gen
         em.close();
 
         return record;
+    }
+
+    @Override
+    public Collection<T> getAllRecords() {
+        final EntityManager em = emf.createEntityManager();
+
+        final TypedQuery<T> query = em.createQuery("from " + workingClass().getName(), workingClass());
+
+        final Collection<T> resultList = query.getResultList();
+        em.close();
+        return resultList;
+    }
+
+    @Override
+    public T getRecordById(long id) {
+        final EntityManager em = emf.createEntityManager();
+
+        final TypedQuery<T> query = em.createQuery("select c from " + workingClass().getName() + " c where c.id = :id", workingClass());
+        query.setParameter("id", id);
+
+        final T singleResult = query.getSingleResult();
+        em.close();
+        return singleResult;
     }
 }
--- src/main/java/dao/jpa/RecordDaoImpl.java
+++ src/main/java/dao/jpa/RecordDaoImpl.java
@@ -3,34 +3,11 @@ package dao.jpa;
 import model.Record;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.util.Collection;
-
 @Repository
 public class RecordDaoImpl extends GenericJpaDaoImpl {
 
     @Override
-    public Collection<Record> getAllRecords() {
-        final EntityManager em = emf.createEntityManager();
-
-        final TypedQuery<Record> query = em.createQuery("from Record", Record.class);
-
-        final Collection<Record> resultList = query.getResultList();
-        em.close();
-        return resultList;
+    protected Class workingClass() {
+        return Record.class;
     }
-
-    @Override
-    public Record getRecordById(long id) {
-        final EntityManager em = emf.createEntityManager();
-
-        final TypedQuery<Record> query = em.createQuery("select c from Record c where c.id = :id", Record.class);
-        query.setParameter("id", id);
-
-        final Record singleResult = query.getSingleResult();
-        em.close();
-        return singleResult;
-    }
-
 }
