6b39794c39f6484082edd1e39454133ec4f21269
Artem Lodygin <aldaer@yandex.ru>
2016-11-19 23:46:57 +0300

    fix Hibernate compatibility problems, make server part working

--- src/main/java/controller/TableServlet.java
+++ src/main/java/controller/TableServlet.java
@@ -4,11 +4,15 @@ package controller;
 import dao.JpaDao;
 import model.SampleRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import utils.JsonNullableGenerator;
 import utils.JsonPacker;
 import utils.RequestObjectParser;
 
 import javax.json.stream.JsonGeneratorFactory;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,6 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
 
+@SuppressWarnings("SpringAutowiredFieldsWarningInspection")
 @WebServlet(name = "TableServlet", urlPatterns = {"/data"})
 public class TableServlet extends HttpServlet {
     @Autowired
@@ -75,4 +80,16 @@ public class TableServlet extends HttpServlet {
     private void setResponseParametersToJson(HttpServletResponse resp) {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
-    }}
+    }
+
+    @Override
+    public void init(final ServletConfig config) throws ServletException {
+        super.init(config);
+        System.out.println("Retrieving Spring context");
+        final WebApplicationContext springContext = WebApplicationContextUtils.getRequiredWebApplicationContext(config.getServletContext());
+        System.out.println("Retrieving bean factory");
+        final AutowireCapableBeanFactory beanFactory = springContext.getAutowireCapableBeanFactory();
+        System.out.println("Autowiring beans");
+        beanFactory.autowireBean(this);
+    }
+}
